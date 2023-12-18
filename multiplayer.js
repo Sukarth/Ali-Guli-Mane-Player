7a978ca4fb64d80429420bd704091701dfa5a32b
Sukarth Acharya <sukarthacharya@gmail.com>
2023-12-18 21:13:52 +0200

    logic working

--- multiplayer.js
+++ multiplayer.js
@@ -1,5 +1,294 @@
 var socket = io("https://v9djdb27-3000.euw.devtunnels.ms/");
 
+var playerIsFlipped;
+var chosen_circlee;
+// window.addEventListener("resize", fontfitter);
+
+
+
+
+
+
+// // // // function fitFont(){
+// // // //   var child1 = document.getElementById('Player1').children[0];
+// // // //   var child2 = document.getElementById('Player2').children[0];
+// // // //   var child3 = document.getElementById('Gamestatus').children[0];
+// // // //   var elem1 = document.getElementById('Player1');
+// // // //   var elem2 = document.getElementById('Player2');
+// // // //   var elem3 = document.getElementById('Gamestatus');
+// // // //   var getFontSize = 0;
+
+  // // // // textFit(child1)
+  // // // // textFit(child2)
+  // // // // textFit(child3)
+  // // // // window.fitText(child1)
+  // // // // window.fitText(child2)
+  // // // // window.fitText(child3)
+
+  // // // child1.style.visibility = 'hidden';
+  // // // child2.style.visibility = 'hidden';
+  // // // child3.style.visibility = 'hidden';
+
+
+  // // // var counter=0
+  // // // // var child=elem;
+  // // // elem = document.getElementById('condftainer');
+  // // // var getFontSize1 = parseFloat(window.getComputedStyle(child1).getPropertyValue('font-size'));
+  // // // var getFontSize2 = parseFloat(window.getComputedStyle(child2).getPropertyValue('font-size'));
+  // // // var getFontSize3 = parseFloat(window.getComputedStyle(child3).getPropertyValue('font-size'));
+  // // // // console.log(child.offsetHeight)
+  // // // // console.log(getFontSize)
+  // // // // console.log(elem.clientHeight)  
+  // // // while(child1.offsetHeight+child2.offsetHeight+child3.offsetHeight>elem.offsetHeight && counter<10){
+  // // //   if (child1.clientHeight==18.8) {
+      
+  // // //   }
+    
+  // // //   console.log('0');
+  // // //   counter = counter + 1
+  // // //   getFontSize1 -= 1;
+  // // //   getFontSize2 -= 1;
+  // // //   getFontSize3 -= 1;
+  // // //   child1.style.fontSize = getFontSize1 + 'px';
+  // // //   child2.style.fontSize = getFontSize2 + 'px';
+  // // //   child3.style.fontSize = getFontSize3 + 'px';
+    
+  // // // }
+
+
+  // // // while(child1.offsetHeight+child2.offsetHeight+child3.offsetHeight<elem.offsetHeight && counter<10){
+  // // //   console.log('0');
+  // // //   counter = counter + 1
+  // // //   getFontSize1 += 1;
+  // // //   getFontSize2 += 1;
+  // // //   getFontSize3 += 1;
+  // // //   child1.style.fontSize = getFontSize1 + 'px';
+  // // //   child2.style.fontSize = getFontSize2 + 'px';
+  // // //   child3.style.fontSize = getFontSize3 + 'px';
+  // // // }
+
+  // // // while(child1.offsetWidth>elem.offsetWidth  && counter<10){
+  // // //   console.log('0');
+  // // //   counter = counter + 1
+  // // //   getFontSize1 -= 1;
+
+  // // //   child1.style.fontSize = getFontSize1 + 'px';
+
+  // // // }
+
+  // // // while(child2.offsetWidth>elem.offsetWidth && counter<10){
+  // // //   console.log('0');
+  // // //   counter = counter + 1
+
+  // // //   getFontSize2 -= 1;
+
+  // // //   child2.style.fontSize = getFontSize2 + 'px';
+
+  // // // }
+
+  // // // while(child3.offsetWidth>elem.offsetWidth && counter<10){
+  // // //   console.log('0');
+  // // //   counter = counter + 1
+
+  // // //   getFontSize3 -= 1;
+
+  // // //   child3.style.fontSize = getFontSize3 + 'px';
+  // // // }
+
+
+// // // //   console.log(child1+child2+child3)
+
+// // // //   while(child1.clientWidth > elem1.clientWidth){
+// // // //     console.log('111');
+// // // //     if (child1.style.fontSize == ""){
+// // // //       getFontSize = getComputedStyle(child1).fontSize;
+// // // //     }
+// // // //     else {
+// // // //       getFontSize = child1.style.fontSize
+// // // //     }
+// // // //     getFontSize = Number(getFontSize.replace('px',''))
+// // // //     getFontSize -= 1;
+// // // //     child1.style.fontSize = getFontSize + 'px';
+// // // //   }
+
+// // // //   while(child1.clientWidth < elem1.clientWidth ){
+// // // //     console.log('110');
+// // // //     if (child1.style.fontSize == ""){
+// // // //       getFontSize = getComputedStyle(child1).fontSize;
+// // // //     }
+// // // //     else {
+// // // //       getFontSize = child1.style.fontSize
+// // // //     }
+// // // //     getFontSize = Number(getFontSize.replace('px',''))
+// // // //     getFontSize += 1;
+// // // //     child1.style.fontSize = getFontSize + 'px';
+// // // //   }
+
+// // // //   while(child1.clientHeight > elem1.clientHeight){
+// // // //     console.log('211');
+// // // //     if (child1.style.fontSize == ""){
+// // // //       getFontSize = getComputedStyle(child1).fontSize;
+// // // //     }
+// // // //     else {
+// // // //       getFontSize = child1.style.fontSize
+// // // //     }
+// // // //     getFontSize = Number(getFontSize.replace('px',''))
+// // // //     getFontSize -= 1;
+// // // //     child1.style.fontSize = getFontSize + 'px';
+// // // //   }
+
+// // // //   while(child1.clientHeight  < elem1.clientHeight){
+// // // //     console.log('210');
+// // // //     if (child1.style.fontSize == ""){
+// // // //       getFontSize = getComputedStyle(child1).fontSize;
+// // // //     }
+// // // //     else {
+// // // //       getFontSize = child1.style.fontSize
+// // // //     }
+// // // //     getFontSize = Number(getFontSize.replace('px',''))
+// // // //     getFontSize += 1;
+// // // //     child1.style.fontSize = getFontSize + 'px';
+// // // //   }
+
+
+// // // //   while(child2.clientWidth > elem2.clientWidth){
+// // // //     console.log('21');
+// // // //     if (child2.style.fontSize == ""){
+// // // //       getFontSize = getComputedStyle(child2).fontSize;
+// // // //     }
+// // // //     else {
+// // // //       getFontSize = child2.style.fontSize
+// // // //     }
+// // // //     getFontSize = Number(getFontSize.replace('px',''))
+// // // //     getFontSize -= 1;
+// // // //     child2.style.fontSize = getFontSize + 'px';
+// // // //   }
+
+
+// // // //   while(child2.clientWidth < elem2.clientWidth){
+// // // //     console.log('20');
+// // // //     if (child2.style.fontSize == ""){
+// // // //       getFontSize = getComputedStyle(child2).fontSize;
+// // // //     }
+// // // //     else {
+// // // //       getFontSize = child2.style.fontSize
+// // // //     }
+// // // //     getFontSize = Number(getFontSize.replace('px',''))
+// // // //     getFontSize += 1;
+// // // //     child2.style.fontSize = getFontSize + 'px';
+// // // //   }
+
+// // // //   while(child2.clientHeight  > elem2.clientHeight){
+// // // //     console.log('21');
+// // // //     if (child2.style.fontSize == ""){
+// // // //       getFontSize = getComputedStyle(child2).fontSize;
+// // // //     }
+// // // //     else {
+// // // //       getFontSize = child2.style.fontSize
+// // // //     }
+// // // //     getFontSize = Number(getFontSize.replace('px',''))
+// // // //     getFontSize -= 1;
+// // // //     child2.style.fontSize = getFontSize + 'px';
+// // // //   }
+
+
+// // // //   while(child2.clientHeight  < elem2.clientHeight){
+// // // //     console.log('20');
+// // // //     if (child2.style.fontSize == ""){
+// // // //       getFontSize = getComputedStyle(child2).fontSize;
+// // // //     }
+// // // //     else {
+// // // //       getFontSize = child2.style.fontSize
+// // // //     }
+// // // //     getFontSize = Number(getFontSize.replace('px',''))
+// // // //     getFontSize += 1;
+// // // //     child2.style.fontSize = getFontSize + 'px';
+// // // //   }
+
+// // // //   while(child3.clientWidth > elem3.clientWidth){
+// // // //     console.log('31');
+// // // //     if (child3.style.fontSize == ""){
+// // // //       getFontSize = getComputedStyle(child3).fontSize;
+// // // //     }
+// // // //     else {
+// // // //       getFontSize = child3.style.fontSize
+// // // //     }
+// // // //     getFontSize = Number(getFontSize.replace('px',''))
+// // // //     getFontSize -= 1;
+// // // //     child3.style.fontSize = getFontSize + 'px';
+// // // //   }
+
+
+// // // //   while(child3.clientWidth < elem3.clientWidth){
+// // // //     console.log('30');
+// // // //     if (child3.style.fontSize == ""){
+// // // //       getFontSize = getComputedStyle(child3).fontSize;
+// // // //     }
+// // // //     else {
+// // // //       getFontSize = child3.style.fontSize
+// // // //     }
+// // // //     getFontSize = Number(getFontSize.replace('px',''))
+// // // //     getFontSize += 1;
+// // // //     child3.style.fontSize = getFontSize + 'px';
+// // // //   }
+
+// // // //   while(child3.clientHeight  > elem3.clientHeight){
+// // // //     console.log('31');
+// // // //     if (child3.style.fontSize == ""){
+// // // //       getFontSize = getComputedStyle(child3).fontSize;
+// // // //     }
+// // // //     else {
+// // // //       getFontSize = child3.style.fontSize
+// // // //     }
+// // // //     getFontSize = Number(getFontSize.replace('px',''))
+// // // //     getFontSize -= 1;
+// // // //     child3.style.fontSize = getFontSize + 'px';
+// // // //   }
+
+// // // //   while(child3.clientHeight < elem3.clientHeight){
+// // // //     console.log('30');
+// // // //     if (child3.style.fontSize == ""){
+// // // //       getFontSize = getComputedStyle(child3).fontSize;
+// // // //     }
+// // // //     else {
+// // // //       getFontSize = child3.style.fontSize
+// // // //     }
+// // // //     getFontSize = Number(getFontSize.replace('px',''))
+// // // //     getFontSize += 1;
+// // // //     child3.style.fontSize = getFontSize + 'px';
+// // // //   }
+
+
+
+// // // //   // child1.style.visibility = 'visible';
+// // // //   // child2.style.visibility = 'visible';
+// // // //   // child3.style.visibility = 'visible';
+// // // //   return
+// // // // }
+
+
+// // // // function fontfitter() {
+// // // //   console.log('fontfitter');
+// // // //   fitFont(document.getElementById('Player1'));
+// // // //   fitFont(document.getElementById('Player2'));
+// // // //   fitFont(document.getElementById('Gamestatus'));
+// // // //   return
+// // // // }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var object = document.getElementById('Gamestatus');
+//   var overlay = document.getElementById('back_board');
+
+//   var objectPosition = object.getBoundingClientRect().top;
+//   var overlayPosition = overlay.getBoundingClientRect().top;
+
+//   if (objectPosition < overlayPosition) {
+//       object.style.width = '100px';
+//       object.style.height = '100px';
+//       object.style.top = '30px';
+//       object.style.left = '30px';
+//   }
+// });
 
 // function createAddItemWindow() {
 
@@ -86,20 +375,43 @@ function someFunction(div) {
   console.log(circle_clicked_id)
   console.log(circle_clicked_num)
   console.log(typeof circle_clicked_num)
-  if (slotsValues[circle_clicked_num] != "0") {
-    if (Number(circle_clicked_num) - 1 < 7){
-      var sdsdsdkl = Number(circle_clicked_num) - 1 + 7
+
+  chosen_circlee = circle_clicked_num
+
+
+
+
+
+  if (slotsValues[chosen_circlee] != "0") {
+    // if (Number(circle_clicked_num) - 1 < 7){
+    //   var sdsdsdkl = Number(circle_clicked_num) - 1 + 7
+    // }
+    // if (Number(circle_clicked_num) - 1 > 7){
+    //   var sdsdsdkl = Number(circle_clicked_num) - 1 - 7
+    // }
+
+    if (playerIsFlipped == true) {
+      if (Number(circle_clicked_num) >= 1 && Number(circle_clicked_num) <= 7) {
+        chosen_circlee = Number(circle_clicked_num) +7
+      }
+      else if (Number(circle_clicked_num) >= 7 && Number(circle_clicked_num) <= 14) {
+        chosen_circlee = Number(circle_clicked_num) -7
+      }
+      console.log(chosen_circlee)
+      console.log("chosen_circlee")
     }
-    if (Number(circle_clicked_num) - 1 > 7){
-      var sdsdsdkl = Number(circle_clicked_num) - 1 - 7
+    else {
+      chosen_circlee = circle_clicked_num
+      console.log(chosen_circlee)
     }
+
     socket.emit("action", {
       // gridIndex: [Number(circle_clicked_num) - 1, sdsdsdkl],
-      gridIndex: Number(circle_clicked_num) - 1,
+      gridIndex: chosen_circlee.toString(),
     });
   }
 
-  return circle_clicked_id
+  return chosen_circlee 
 };
 
 
@@ -308,8 +620,13 @@ var chosen_circle;
 
 
 socket.on("gameState", function(state) {
+  console.log("diiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+  console.log(state)
   console.log(state.slotBoard)
   console.log(slotsValues)
+  if (state.flippedPlayer.id == socket.id ) {
+    playerIsFlipped = true
+  }
   // slotsValues = state.slotBoard
   // for (let index = 0; index < state.board.length; index++) {
   //   const player = state.board[index];
@@ -319,31 +636,74 @@ socket.on("gameState", function(state) {
   //     slots_chosen[index+1] = null;
   //   }
   // }
-// var counter = 1
-//   if (state.flippedPlayer.id == socket.id){
-//     for( var sloot in state.slotBoard) {
-//       counter = counter + 1
-//       if (counter > 7){
-        
-//     }
-//   }
-//   console.log(slotsValues)
+  // var counter = 1
+  //   if (state.flippedPlayer.id == socket.id){
+  //     for( var sloot in state.slotBoard) {
+  //       counter = counter + 1
+  //       if (counter > 7){
+          
+  //     }
+  //   }
+  //   console.log(slotsValues)
 
   console.log(state.players.length)
   
   if (state.players.length == 2) {
-    if (state.flippedPlayer.id == socket.id){
+    if (state.flippedPlayer.id == socket.id && state.currentPlayer == socket.id){
       for (var key in state.slotBoard) {
         var numericKey = parseInt(key);
-        if (numericKey < 7) {
+        if (numericKey == 7) {
+          slotsValues[(numericKey + 7).toString()] = state.slotBoard[key];
+        }
+        else if (numericKey == 14) {
+          slotsValues[(numericKey - 7).toString()] = state.slotBoard[key];
+        }
+        else if (numericKey < 7) {
           slotsValues[(numericKey + 7).toString()] = state.slotBoard[key];
         } else if (numericKey > 7) {
           slotsValues[(numericKey - 7).toString()] = state.slotBoard[key];
-        } else {
-          slotsValues[key] = state.slotBoard[key];
         }
+        // else {
+        //   slotsValues[key] = state.slotBoard[key];
+        // }
       }
       console.log(slotsValues)
+      let sum = Object.values(slotsValues).reduce((acc, val) => acc + parseInt(val), 0);
+
+      console.log("Sum of values:", sum);
+    }
+    else if (state.flippedPlayer.id != socket.id && state.currentPlayer != socket.id) {
+      for (var key in state.slotBoard) {
+        var numericKey = parseInt(key);
+        if (numericKey == 7) {
+          slotsValues[(numericKey + 7).toString()] = state.slotBoard[key];
+        }
+        else if (numericKey == 14) {
+          slotsValues[(numericKey - 7).toString()] = state.slotBoard[key];
+        }
+        else if (numericKey < 7) {
+          slotsValues[(numericKey + 7).toString()] = state.slotBoard[key];
+        } else if (numericKey > 7) {
+          slotsValues[(numericKey - 7).toString()] = state.slotBoard[key];
+        }
+        // else {
+        //   slotsValues[key] = state.slotBoard[key];
+        // }
+      }
+      console.log(slotsValues)
+      let sum = Object.values(slotsValues).reduce((acc, val) => acc + parseInt(val), 0);
+
+      console.log("Sum of values:", sum);
+    }
+
+    else if (state.flippedPlayer.id == socket.id && state.currentPlayer != socket.id){
+      for (var key in state.slotBoard) {
+        slotsValues[key] = state.slotBoard[key];
+      }
+      console.log(slotsValues)
+      let sum = Object.values(slotsValues).reduce((acc, val) => acc + parseInt(val), 0);
+
+      console.log("Sum of values:", sum);
     }
   }
 
@@ -355,21 +715,21 @@ socket.on("gameState", function(state) {
   // }
 
 
-  document.getElementById("Gamestatus").innerHTML = "";
+  document.getElementById("Gamestatus").children[0].innerHTML = " ";
   switch (state.result.status) {
     case Statuses.WAITING:
-      document.getElementById("Gamestatus").innerHTML = "Waiting for players....";
+      document.getElementById("Gamestatus").children[0].innerHTML = "Waiting for players....";
       break;
     case Statuses.PLAYING:
-      document.getElementById("Gamestatus").innerHTML = state.currentPlayer.playerName + " to play";
+      document.getElementById("Gamestatus").children[0].innerHTML = state.currentPlayer.playerName + " to play";
       break;
     case Statuses.DRAW:
-      document.getElementById("Gamestatus").innerHTML = "Draw!";
+      document.getElementById("Gamestatus").children[0].innerHTML = "Draw!";
       document.getElementById("rematch").style.visibility = "visible"
       canRematch = true
       break;
     case Statuses.WIN:
-      document.getElementById("Gamestatus").innerHTML = state.result.winner.playerName + " Wins!";
+      document.getElementById("Gamestatus").children[0].innerHTML = state.result.winner.playerName + " Wins!";
       document.getElementById("rematch").style.visibility = "visible"
       canRematch = true
       break;
@@ -377,15 +737,52 @@ socket.on("gameState", function(state) {
       break;
   }
 
-  document.getElementById("Player1").innerHTML = "";
-  document.getElementById("Player2").innerHTML = "";
+  document.getElementById("Player1").children[0].innerHTML = " ";
+  document.getElementById("Player2").children[0].innerHTML = " ";
   if (state.players.length > 0) {
-    document.getElementById("Player1").innerHTML = "Player 1: " + state.players[0].playerName;
+    document.getElementById("Player1").children[0].innerHTML = "Player 1: " + state.players[0].playerName;
   }
 
   if (state.players.length > 1) {
-    document.getElementById("Player2").innerHTML = "Player 2: " + state.players[1].playerName;
+    document.getElementById("Player2").children[0].innerHTML = "Player 2: " + state.players[1].playerName;
   }
+
+
+
+
+  for (const thingies in slotsValues) {
+    console.log(slotsValues[thingies])
+    var parent = document.getElementById("divCircle"+thingies);
+    parent.replaceChildren()
+    for (var i = 0; i < Number(slotsValues[thingies]); i++) {
+      var div = document.createElement("span");
+      // div.style.width = "15px";
+      // div.style.height = "15px";
+      div.style.backgroundColor = "red";
+      div.style.borderRadius = "50%";
+      // div.style.display = "inline-block";
+      div.className = "rtryd";
+      
+      parent.appendChild(div);
+      // document.getElementById("circle"+thingies).appendChild(div);
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
